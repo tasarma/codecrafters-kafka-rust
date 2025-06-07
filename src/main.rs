86e5c3859b888e96b582c9293a5d07b03c8203df
tasarma <tasarm7@protonmail.com>
2025-06-07 17:10:23 +0200

    parse the request_api_version field and respond with an error code

--- src/main.rs
+++ src/main.rs
@@ -19,19 +19,31 @@ fn main() {
                 let mut buffer = [0u8; 1024];
                 // How many actual bytes we got
                 let bytes_read = stream.read(&mut buffer).unwrap();
-                println!("Received {} bytes", bytes_read);
+                //println!("Received {} bytes", bytes_read);
 
                 let mut cursor = Cursor::new(&buffer[..bytes_read]);
 
                 let _message_size = cursor.read_u32::<BigEndian>().unwrap();
-                let _api_key = cursor.read_u16::<BigEndian>().unwrap();
-                let _request_api_version = cursor.read_u16::<BigEndian>().unwrap();
+                let _request_api_key = cursor.read_u16::<BigEndian>().unwrap();
+                let request_api_version = cursor.read_u16::<BigEndian>().unwrap();
                 let correlation_id = cursor.read_u32::<BigEndian>().unwrap();
+
+                //println!("message_size: {:?}", _message_size);
+                //println!("request_api_key: {:?}", _request_api_key);
+                //println!("request_api_version: {:?}", request_api_version);
+                //println!("correlation_id: {:?}", correlation_id);
 
                 let mut response = Vec::new();
                 response.write_u32::<BigEndian>(0).unwrap();
                 response.write_u32::<BigEndian>(correlation_id).unwrap();
-                println!("{:?}", response);
+
+                if (0..=4).contains(&request_api_version) {
+                    response.write_u16::<BigEndian>(0).unwrap(); // No error
+                } else {
+                    response.write_u16::<BigEndian>(35).unwrap(); // UNSUPPORTED_VERSION
+                }
+
+                //println!("{:?}", response);
 
                 stream.write_all(&response).unwrap();
             }
