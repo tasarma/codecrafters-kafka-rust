1a1fa11294c3fb57940a9b5b03d80ad7c65bd662
tasarma <tasarm7@protonmail.com>
2025-06-08 22:29:01 +0200

    implement the response body for the APIVersions request

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,8 @@ use std::{
 };
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
 
     for stream in listener.incoming() {
@@ -19,31 +17,42 @@ fn main() {
                 let mut buffer = [0u8; 1024];
                 // How many actual bytes we got
                 let bytes_read = stream.read(&mut buffer).unwrap();
-                //println!("Received {} bytes", bytes_read);
 
                 let mut cursor = Cursor::new(&buffer[..bytes_read]);
 
                 let _message_size = cursor.read_u32::<BigEndian>().unwrap();
                 let _request_api_key = cursor.read_u16::<BigEndian>().unwrap();
-                let request_api_version = cursor.read_u16::<BigEndian>().unwrap();
+                let _request_api_version = cursor.read_u16::<BigEndian>().unwrap();
                 let correlation_id = cursor.read_u32::<BigEndian>().unwrap();
 
-                //println!("message_size: {:?}", _message_size);
-                //println!("request_api_key: {:?}", _request_api_key);
-                //println!("request_api_version: {:?}", request_api_version);
-                //println!("correlation_id: {:?}", correlation_id);
+                let mut response_body = Vec::new();
+
+                response_body
+                    .write_u32::<BigEndian>(correlation_id)
+                    .unwrap();
+
+                response_body
+                    .write_u16::<BigEndian>(0) // 0 means No error
+                    .unwrap();
+
+                // Support 1 API key
+                response_body.write_u8(1).unwrap();
+
+                // API Key: 18 with min and max supported api versions
+                response_body.write_u16::<BigEndian>(18).unwrap();
+                response_body.write_u16::<BigEndian>(0).unwrap();
+                response_body.write_u16::<BigEndian>(4).unwrap();
+
+                // ThrottleTimeMs = 0 for v4
+                response_body.write_u32::<BigEndian>(0).unwrap();
 
                 let mut response = Vec::new();
-                response.write_u32::<BigEndian>(0).unwrap();
-                response.write_u32::<BigEndian>(correlation_id).unwrap();
+                let response_message_size = response_body.len() as u32;
 
-                if (0..=4).contains(&request_api_version) {
-                    response.write_u16::<BigEndian>(0).unwrap(); // No error
-                } else {
-                    response.write_u16::<BigEndian>(35).unwrap(); // UNSUPPORTED_VERSION
-                }
-
-                //println!("{:?}", response);
+                response
+                    .write_u32::<BigEndian>(response_message_size)
+                    .unwrap();
+                response.extend_from_slice(&response_body);
 
                 stream.write_all(&response).unwrap();
             }
